b8a0d04ffa463dded21067ce80168903f341a146
aks02303 <129094451+aks02303@users.noreply.github.com>
2024-12-14 23:22:42 +0530

    Update layout.tsx

--- striide-frontend/src/app/home/layout.tsx
+++ striide-frontend/src/app/home/layout.tsx
@@ -1,14 +1,14 @@
-import type { Metadata } from "next";
+// import type { Metadata } from "next";
 
-export const metadata: Metadata = {
-    title: "Striide | Home",
-    description: "Welcome back, Ready For Your Next Destination?",
-};
+// export const metadata: Metadata = {
+//     title: "Striide | Home",
+//     description: "Welcome back, Ready For Your Next Destination?",
+// };
 
-export default function LandingLayout({
-    children,
-}: {
-    children: React.ReactNode;
-}) {
-    return <section className="">{children}</section>;
-}
+// export default function LandingLayout({
+//     children,
+// }: {
+//     children: React.ReactNode;
+// }) {
+//     return <section className="">{children}</section>;
+// }
