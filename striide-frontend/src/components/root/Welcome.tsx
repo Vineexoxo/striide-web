6b60b82e86fbd95ff0a8c6b9b8bfc51f35c5bb50
Vineexoxo <vineetpriyedarshi@gmail.com>
2024-12-15 11:40:43 +0530

    live patching

--- striide-frontend/src/components/root/Welcome.tsx
+++ striide-frontend/src/components/root/Welcome.tsx
@@ -63,6 +63,7 @@ const WelcomePage = () => {
                         className="italic"
                     >
                         Priide
+                        
                     </motion.span>
                 </div>
                 <motion.h2
